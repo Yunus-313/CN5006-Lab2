649e18bf2cf1659675c0ecde5ed568d5dc3490d0
Yunus-313 <133156553+Yunus-313@users.noreply.github.com>
2024-10-08 17:56:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('This is my first program');
+console.log('Welcome John, your salary is 50000');
+const num1 = 10;
+const num2 = 2;
+const sum = (num1 + num2); //Adding the two numbers
+//Program for the output of the two numbers:
+console.log('The sum of ' + num1 + ' and ' + num2 ,'is ' + sum); 
+
+//Program for the output of adding, subtracting, multiplying and dividing the two numbers:
+console.log(num1 ,'divided by ' + num2 , ' is ' + (num1/num2));
+console.log(num1 ,'multiplied by ' + num2 , ' is ' + (num1*num2));
+console.log(num1 ,'plus ' + num2 , ' is ' + (sum));
+console.log(num1 ,'minus ' + num2 , ' is ' + (num1 - num2));
